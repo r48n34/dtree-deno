baf573be5b811a78a934e43088760f08d2410f88
reemo <r48n34@gmail.com>
2023-06-22 02:30:46 +0800

    Update interface.ts

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -1,12 +1,4 @@
-import type { Stats } from "node:fs";
-
 export interface Nodes {
     label: string
     nodes: Nodes[]
 }
-
-export interface KlawSyncObject {
-    path: string
-    stats: Stats
-}
-
